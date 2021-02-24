9306559fc5b4166cc6c3b05e1a061eedc880d145
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-02-24 22:52:55 +0900

    コントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :items
+         validates :nick_name, presence: true
+         validates :family_name, presence: true
+         validates :first_name, presence: true
+         validates :family_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birth_day, presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :password,length:{minimum:6},with: PASSWORD_REGEX
   has_many :purchases
 end
