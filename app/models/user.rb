19bb31151727a71ef85e6707c64ee0102f5e117d
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-02-26 19:19:21 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nick_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
-  validates_format_of :family_name, with: NAME_REGEX
-  validates_format_of :first_name, with: NAME_REGEX
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates_format_of :family_name_kana, with: KANA_REGEX
-  validates_format_of :first_name_kana, with: KANA_REGEX
-  validates :birth_day, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
-  has_many :purchases
+  with_options presence: true do
+   validates :nick_name
+   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+   validates_format_of :family_name, with: NAME_REGEX
+   validates_format_of :first_name, with: NAME_REGEX
+   KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+   validates_format_of :family_name_kana, with: KANA_REGEX
+   validates_format_of :first_name_kana, with: KANA_REGEX
+   validates :birth_day
+  end
+   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+   validates_format_of :password, with: PASSWORD_REGEX
+   has_many :purchases
 end
