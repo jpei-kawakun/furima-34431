b07a065fcfe8ab27a922fb34b6e9656632fd0163
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-02-26 20:58:20 +0900

    ImageMagickインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :items
-  resources :prototypes, only: :index
+  resources :prototypes, only: [:index, :new, :create]
   root to: "items#index"
   devise_for :users
 end
