1eb3e0a3bde7d57133469fb05c749b5626fc07f5
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-03-01 19:16:47 +0900

    ActiveHashアソシエーション

--- app/models/category.rb
+++ app/models/category.rb
@@ -13,4 +13,8 @@ class Category < ActiveHash::Base
     { id: 11, name: 'その他' }
 
   ]
+
+  include ActiveHash::Associations
+  has_many :item
+  
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -20,4 +20,11 @@ class Item < ApplicationRecord
     belongs_to :user
     has_one :purchase
     has_one_attached :image
+    
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :prefecture
+    belongs_to :shipping_cost
+    belongs_to :shipping_day
+    belongs_to :status
 end
