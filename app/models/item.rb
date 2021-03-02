d5b28dad4202da464943496471fca40781d538a7
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-03-02 16:26:15 +0900

    itemバリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,11 +22,13 @@ class Item < ApplicationRecord
      validates :prefecture_id
     end
     
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 0 } do
      validates :status_id
      validates :category_id
      validates :shipping_cost_id
      validates :shipping_day_id
+     validates :prefecture_id
+
     end
 
 
