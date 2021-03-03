800c1c522a9a102724740d88cab5c74fcdc4172f
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-03-03 21:57:54 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -23,6 +25,11 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id == @item.user_id
+      redirect_to edit_item_path
+     else 
+       redirect_to root_path
+     end
   end
 
   def update
@@ -36,9 +43,17 @@ class ItemsController < ApplicationController
 
   private
 
-  def item_params
-    params.require(:item).permit(:image,:name,:description,:category_id,:prefecture_id,:shipping_cost_id,:shipping_day_id,:status_id,:price).merge(user_id: current_user.id)
+   def item_params
+     params.require(:item).permit(:image,:name,:description,:category_id,:prefecture_id,:shipping_cost_id,:shipping_day_id,:status_id,:price).merge(user_id: current_user.id)
+   end
+
+   def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
+
+  
 
 end
 
