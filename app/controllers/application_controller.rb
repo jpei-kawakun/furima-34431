66440c4137af556e832926a5d2879149e36bb3e9
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-03-08 17:41:09 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
 
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana,
